ec55eb424e04a75329dcd7d5642ff5731ddf98b5
m-h-thomas <mhthomas.8193@gmail.com>
2025-03-27 13:37:35 -0600

    added filter search functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,18 +2,41 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [movie, setMovie] = useState([]);
 
+  //Set movie in state
+  const [movie, setMovie] = useState([]);
+  const [search, setSearch] = useState("");
+
+  //Fetch movie list from database
   useEffect(() => {
     fetch(`http://localhost:8080/movies`)
     .then(res => res.json())
     .then(movie => setMovie(movie))
   }, [])
 
+  //Handle search
+  const filteredMovies = movie.filter((movie) =>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+)
+
   return (
     <>
+
+      {/* Search bar */}
+      <div className="search-input">
+        <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </div>
+
       <h1>Movie List</h1>
-      {(movie.map((movie, index) => <p key={index}>{movie.title}</p>))}
+
+      {(filteredMovies.map((movie, index) => <p key={index}>{movie.title}</p>))}
+
     </>
   )
 }
